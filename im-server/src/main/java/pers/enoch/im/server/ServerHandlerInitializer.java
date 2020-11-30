12180c9f4ad12540b283afd1301cf7768cc9764e
yang <yang.zhao@ximalaya.com>
2020-11-30 17:03:17 +0800

    创建NettyServer

---
+++ im-server/src/main/java/pers/enoch/im/server/ServerHandlerInitializer.java
@@ -0,0 +1,18 @@
+package pers.enoch.im.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+
+/**
+ * @Author yang.zhao
+ * @Date 2020/11/30 16:22
+ * @Version 1.0
+ * @Description: Handler初始化
+ **/
+public class ServerHandlerInitializer extends ChannelInitializer<Channel> {
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        // 1. 解析http
+        // 2.
+    }
+}
